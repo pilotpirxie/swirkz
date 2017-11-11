644c08dec1743e923a38bf43c684e43b204a8603
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2017-11-11 11:51:55 +0100

    Swirkz class

--- lib/SwirkzController.php
+++ lib/SwirkzController.php
@@ -5,10 +5,10 @@ class Swirkz {
       // query that check if message exist and|or was created in last 180 minutes
       $result = $link->query("SELECT TIMESTAMPDIFF( MINUTE, create_date, CURRENT_TIMESTAMP ) as diff FROM messages WHERE room_url = '$room_id' GROUP BY create_date HAVING diff <= 180 ORDER BY create_date DESC LIMIT 1");
 
-      if ($result->num_rows == 0){
-        return false;
-      } else {
+      if ($result->num_rows > 0){
         return true;
+      } else {
+        return false;
       }
     }
 }
